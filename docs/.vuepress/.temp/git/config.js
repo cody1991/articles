8eb062dd210e45038f23dd1e1d1c7dbe6854f2fe
codytang <codytang@tencent.com>
2025-12-09 14:51:43 +0100

    build: 移除不应该提交的 VuePress 缓存文件
    - 从版本控制中移除 docs/.vuepress/.temp/ 文件
    - 从版本控制中移除 docs/.vuepress/dist/ 文件
    - 这些是构建产物和缓存，应该被 .gitignore 忽略

--- docs/.vuepress/.temp/git/config.js
+++
@@ -1,9 +0,0 @@
-import { GitContributors } from "/Users/cody/CodeBuddy/20251209001439/node_modules/@vuepress/plugin-git/lib/client/components/GitContributors.js";
-import { GitChangelog } from "/Users/cody/CodeBuddy/20251209001439/node_modules/@vuepress/plugin-git/lib/client/components/GitChangelog.js";
-
-export default {
-  enhance: ({ app }) => {
-    app.component("GitContributors", GitContributors);
-    app.component("GitChangelog", GitChangelog);
-  },
-};
